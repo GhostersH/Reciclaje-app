4ab725faaff6d7cf086faa8e838684972aafb197
GhostersH <97006291+GhostersH@users.noreply.github.com>
2023-02-08 19:56:50 -0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+
+
+    domains: ['http://localhost:1337/uploads/']
+  }
+}
+
+module.exports = nextConfig
